2c508868f6c733fc2047ac13836bc7c24b0bb6d6
AyaanPathan-codes <ayaanpathanra@gmail.com>
2024-10-26 14:27:52 +0530

    release 1

--- app.js
+++ app.js
@@ -6,32 +6,49 @@ let thumbnails = document.querySelectorAll('.thumbnail .item');
 // config param
 let countItem = items.length;
 let itemActive = 0;
+
 // event next click
-next.onclick = function(){
-    itemActive = itemActive + 1;
-    if(itemActive >= countItem){
-        itemActive = 0;
-    }
+next.onclick = function() {
+    itemActive = (itemActive + 1) % countItem;
     showSlider();
 }
-//event prev click
-prev.onclick = function(){
-    itemActive = itemActive - 1;
-    if(itemActive < 0){
-        itemActive = countItem - 1;
-    }
+
+// event prev click
+prev.onclick = function() {
+    itemActive = (itemActive - 1 + countItem) % countItem;
     showSlider();
 }
-// auto run slider
-let refreshInterval = setInterval(() => {
-    next.click();
-}, 5000)
-function showSlider(){
+
+// auto run slider, only on larger screens
+let refreshInterval;
+function startAutoSlider() {
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000);
+}
+
+// Function to stop auto slider
+function stopAutoSlider() {
+    clearInterval(refreshInterval);
+}
+
+// auto run slider if screen width is larger than a certain value
+if (window.innerWidth > 768) { // Example breakpoint
+    startAutoSlider();
+}
+
+// show slider function
+function showSlider() {
     // remove item active old
     let itemActiveOld = document.querySelector('.slider .list .item.active');
     let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
-    itemActiveOld.classList.remove('active');
-    thumbnailActiveOld.classList.remove('active');
+
+    if (itemActiveOld) {
+        itemActiveOld.classList.remove('active');
+    }
+    if (thumbnailActiveOld) {
+        thumbnailActiveOld.classList.remove('active');
+    }
 
     // active new item
     items[itemActive].classList.add('active');
@@ -39,12 +56,14 @@ function showSlider(){
     setPositionThumbnail();
 
     // clear auto time run slider
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(() => {
-        next.click();
-    }, 5000)
+    stopAutoSlider();
+    if (window.innerWidth > 768) { // Check again before restarting auto slider
+        startAutoSlider();
+    }
 }
-function setPositionThumbnail () {
+
+// function to set position of thumbnail
+function setPositionThumbnail() {
     let thumbnailActive = document.querySelector('.thumbnail .item.active');
     let rect = thumbnailActive.getBoundingClientRect();
     if (rect.left < 0 || rect.right > window.innerWidth) {
@@ -58,4 +77,13 @@ thumbnails.forEach((thumbnail, index) => {
         itemActive = index;
         showSlider();
     })
-})
+});
+
+// Listen for window resize to adjust auto slider behavior
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        startAutoSlider();
+    } else {
+        stopAutoSlider(); // Stop auto slider on small screens
+    }
+});
